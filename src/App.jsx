b17dce7a736f8dcc7297d86f26177a08bf040839
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-06-28 20:03:59 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,52 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
+import { useEffect } from "react";
+import { useState } from "react";
+
 export default function App() {
+  const [amount, setAmount] = useState(1);
+  const [fromCurrency, setFromCurrency] = useState("EUR");
+  const [toCurrency, setToCurrency] = useState("USD");
+  const [converted, setConverted] = useState("");
+
+  useEffect(() => {
+    async function convert() {
+      if (fromCurrency === toCurrency) {
+        setConverted(amount);
+        return;
+      }
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`
+      );
+      const data = await res.json();
+      setConverted(data.rates[toCurrency]);
+      console.log(converted);
+    }
+    convert();
+  }, [amount, fromCurrency, toCurrency]);
+
   return (
     <div>
-      <input type="text" />
-      <select>
+      <input type="text" value={amount} onChange={(e) => setAmount(e.target.value)}/>
+      <select
+        value={fromCurrency}
+        onChange={(e) => setFromCurrency(e.target.value)}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+      <select
+        value={toCurrency}
+        onChange={(e) => setToCurrency(e.target.value)}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>OUTPUT</p>
+      <p>{converted}</p>
     </div>
   );
 }
